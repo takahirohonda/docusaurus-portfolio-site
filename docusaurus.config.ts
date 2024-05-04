aee79fafa7984afc7d0ced17272082d8d7cb8d17
mydatahack <thon6thon6@hotmail.com>
2024-05-05 09:41:12 +1000

    Update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -127,7 +127,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Takahiro Honda`,
     },
     prism: {
       theme: prismThemes.github,
