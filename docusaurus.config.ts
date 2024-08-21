08f14bd91fa31779cb170e31f3a684a60dcf054d
mydatahack <thon6thon6@hotmail.com>
2024-08-21 08:33:38 +0200

    Update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   organizationName: "mdh", // Usually your GitHub org/user name.
   projectName: "docusaurus-portfolio-site", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn", // having trouble with gitpage...
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
