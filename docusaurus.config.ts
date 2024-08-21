8ed7a081063f835cc9320054cce8206ce0842ca4
mydatahack <thon6thon6@hotmail.com>
2024-08-21 08:09:48 +0200

    Update url for gitpage deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
   favicon: "img/icon-circle.png",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://takahirohonda.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/tk-blog",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
