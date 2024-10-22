cc0e5447bb252f052c592742ee75c19fc0aec2cd
mydatahack <thon6thon6@hotmail.com>
2024-10-23 10:09:40 +1100

    add missing update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
-import "react-image-gallery/styles/css/image-gallery.css";
+
 import styles from "./index.module.css";
 import { WirSuchenDichFromInternet } from "../components/BlogSpecific/WirSuchenDich/WirSuchenDichFromInternet";
 
