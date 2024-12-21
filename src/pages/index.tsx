98c20aac8dca2ac11a0a8174f7980ace132e74ed
mydatahack <thon6thon6@hotmail.com>
2024-12-22 09:01:06 +1100

    feat: update home page with YouTube iframes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,52 @@ export default function Home(): JSX.Element {
       // Description will go into a meta tag in <head />
       description="blog, portfolio, art, music, creative writing, life hack"
     >
-      <main className={clsx(`bg-black text-white flex flex-col grow `)}>
-        <div className="container flex flex-col justify-center items-center grow">
-          <div className="">content here</div>
+      <main className={clsx(`home-bg text-white flex flex-col grow `)}>
+        <div className="container flex flex-col justify-center items-center grow mt-[24px]">
+          <h1>Hello World! </h1>
+          <div
+            className={clsx(
+              `
+              w-full
+              flex
+              flex-col
+              gap-[16px]
+              xl:justify-evenly
+              xl:gap-[0px]
+              xl:flex-row
+            `
+            )}
+          >
+            <div className="w-[560px] h-[315px]">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/videoseries?si=b6hnLyz4gx-mbcBt&amp;list=PLl3_a4EzgdPozGQyARnTZzjLbCmm6_25N"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              />
+            </div>
+            <div className="w-[560px] h-[315px]">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/uDbUE3jxxqE?si=F9rby9AK1qEwVjHj"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              />
+            </div>
+          </div>
+          <h1>About</h1>
+          <img
+            src="img/home/photoversary-1920x1358.jpeg"
+            alt="main image totoro"
+          />
         </div>
       </main>
     </Layout>
