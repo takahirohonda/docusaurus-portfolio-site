1cd33b8fb38712488ce06a765584c02c9080bbd8
mydatahack <thon6thon6@hotmail.com>
2024-12-22 09:23:55 +1100

    Remove unnecessary outer iframe divs

--- my-learning-notes/current/95-quick-css-tailwind-reminder/index.md
+++ my-learning-notes/current/95-quick-css-tailwind-reminder/index.md
@@ -43,3 +43,68 @@ sidebar_position: 96
 This is so far the best cheat sheet for flex: https://css-tricks.com/snippets/css/a-guide-to-flexbox/.
 
 </details>
+
+<details>
+<summary>
+  3. What are the default breakpoints in tailwind?
+</summary>
+
+```
+sm (640px) max-width: 640px;
+md (768px) max-width: 768px;
+lg (1024px) max-width: 1024px;
+xl (1280px) max-width: 1280px;
+2xl (1536px) max-width: 1536px;
+```
+
+We can add new breakpoints like this:
+
+```ts
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '1920px',
+      },
+    },
+  },
+  plugins: [],
+}
+```
+
+Other breakpoint examples...
+
+```ts
+export const BREAKPOINT = {
+  XS: 640, // tablet - portrait
+  SM: 768, // tablet - landscape
+  MD: 1024, // laptop - smaller
+  LG: 1280, // laptop - medium
+  XL: 1536, // desktop
+  XXL: 1920, // desktop - extra wide
+} as const
+```
+
+</details>
+
+<details>
+<summary>
+  4. What are the differences between replaced elements (like iframe, img, video, embed, fencedframe) and block level containers (like div, p)
+
+</summary>
+
+**Replaced Elements**
+
+- Has an intrinsic size defined by its content or attributes (e.g., width and height).
+- Won't automatically stretch to fit its container unless something like width 100% is applied.
+- Isolated and rendered separately by the browser, making it less responsive to flexbox rules without additional CSS.
+
+https://developer.mozilla.org/en-US/docs/Web/CSS/Replaced_element
+
+**Block Elements**
+
+- A div is a non-replaced block-level element that follows standard CSS rules for layout.
+- A div automatically stretches to the width of its parent unless constrained by styles (e.g., max-width, flex-basis, or width).
+- It works well with flexbox, grid, and other layout systems because it has no intrinsic size.
+
+</details>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import clsx from "clsx";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
+import clsx from 'clsx'
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
       title={`Home | ${siteConfig.title}`}
@@ -25,30 +25,27 @@ export default function Home(): JSX.Element {
             `
             )}
           >
-            <div className="w-[560px] h-[315px]">
-              <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/videoseries?si=b6hnLyz4gx-mbcBt&amp;list=PLl3_a4EzgdPozGQyARnTZzjLbCmm6_25N"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-              />
-            </div>
-            <div className="w-[560px] h-[315px]">
-              <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/uDbUE3jxxqE?si=F9rby9AK1qEwVjHj"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-              />
-            </div>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/videoseries?si=b6hnLyz4gx-mbcBt&amp;list=PLl3_a4EzgdPozGQyARnTZzjLbCmm6_25N"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            />
+
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/uDbUE3jxxqE?si=F9rby9AK1qEwVjHj"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            />
           </div>
           <h1>About</h1>
           <img
@@ -58,5 +55,5 @@ export default function Home(): JSX.Element {
         </div>
       </main>
     </Layout>
-  );
+  )
 }
