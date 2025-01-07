4cef343a7a793030a2f98857ad12d78e3612b399
mydatahack <thon6thon6@hotmail.com>
2025-01-07 12:08:01 +1100

    ci: deploy to root of github page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,107 +1,109 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import type { Config } from "@docusaurus/types";
-import type * as Preset from "@docusaurus/preset-classic";
-import tailwindPlugin from "./plugins/tailwind-config.cjs";
+import { themes as prismThemes } from 'prism-react-renderer'
+import type { Config } from '@docusaurus/types'
+import type * as Preset from '@docusaurus/preset-classic'
+import tailwindPlugin from './plugins/tailwind-config.cjs'
 
 const config: Config = {
-  title: "Art is the sublimation of shitty experiences.",
+  title: 'Art is the sublimation of shitty experiences.',
   // tagline: "Art is the sublimation of shitty experiences.",
-  favicon: "img/icon-circle.png",
+  favicon: 'img/icon-circle.png',
 
   // Set the production url of your site here
-  url: "https://takahirohonda.github.io/",
+  url: 'https://takahirohonda.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/tk-blog",
+  // baseUrl: "/tk-blog",
+  // deploying this project to the root of my GitHub page
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "mdh", // Usually your GitHub org/user name.
-  projectName: "docusaurus-portfolio-site", // Usually your repo name.
+  organizationName: 'mdh', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-portfolio-site', // Usually your repo name.
 
-  onBrokenLinks: "warn", // having trouble with git page...
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'warn', // having trouble with git page...
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   plugins: [
     tailwindPlugin,
     [
-      "@docusaurus/plugin-content-blog",
+      '@docusaurus/plugin-content-blog',
       {
-        id: "blogs-english",
-        routeBasePath: "my-blogs",
-        path: "./blogs-english",
+        id: 'blogs-english',
+        routeBasePath: 'my-blogs',
+        path: './blogs-english',
         showReadingTime: true,
       },
     ],
     [
-      "@docusaurus/plugin-content-blog",
+      '@docusaurus/plugin-content-blog',
       {
-        id: "blogs-japanese",
-        routeBasePath: "my-japanese-blogs",
-        path: "./blogs-japanese",
+        id: 'blogs-japanese',
+        routeBasePath: 'my-japanese-blogs',
+        path: './blogs-japanese',
         showReadingTime: true,
       },
     ],
     [
-      "@docusaurus/plugin-content-docs",
+      '@docusaurus/plugin-content-docs',
       {
-        id: "my-projects",
-        path: "my-projects",
-        routeBasePath: "my-projects",
-        sidebarPath: "./sidebars/sidebars.ts",
+        id: 'my-projects',
+        path: 'my-projects',
+        routeBasePath: 'my-projects',
+        sidebarPath: './sidebars/sidebars.ts',
       },
     ],
     [
-      "@docusaurus/plugin-content-docs",
+      '@docusaurus/plugin-content-docs',
       {
-        id: "my-writings",
-        path: "my-writings",
-        routeBasePath: "my-writings",
-        sidebarPath: "./sidebars/sidebars.ts",
+        id: 'my-writings',
+        path: 'my-writings',
+        routeBasePath: 'my-writings',
+        sidebarPath: './sidebars/sidebars.ts',
       },
     ],
     [
-      "@docusaurus/plugin-content-docs",
+      '@docusaurus/plugin-content-docs',
       {
-        id: "music-projects",
-        path: "music-projects",
-        routeBasePath: "music-projects",
-        sidebarPath: "./sidebars/sidebars.ts",
+        id: 'music-projects',
+        path: 'music-projects',
+        routeBasePath: 'music-projects',
+        sidebarPath: './sidebars/sidebars.ts',
       },
     ],
     [
-      "@docusaurus/plugin-content-docs",
+      '@docusaurus/plugin-content-docs',
       {
-        id: "my-learning-notes",
-        path: "my-learning-notes",
-        routeBasePath: "my-learning-notes",
-        sidebarPath: "./sidebars/sidebars.ts",
+        id: 'my-learning-notes',
+        path: 'my-learning-notes',
+        routeBasePath: 'my-learning-notes',
+        sidebarPath: './sidebars/sidebars.ts',
       },
     ],
   ],
 
   presets: [
     [
-      "classic",
+      'classic',
       {
         docs: {
-          sidebarPath: "./sidebars/sidebars.ts",
-          path: "docs",
+          sidebarPath: './sidebars/sidebars.ts',
+          path: 'docs',
         },
         blog: {
-          id: "1",
+          id: '1',
           showReadingTime: true,
         },
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
@@ -109,88 +111,88 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: "Taka Honda",
+      title: 'Taka Honda',
       logo: {
-        alt: "My Site Logo",
-        src: "img/icon-circle.png",
+        alt: 'My Site Logo',
+        src: 'img/icon-circle.png',
       },
       items: [
         {
-          label: "My Projects",
-          to: "my-projects/intro",
-          position: "left",
-          docsPluginId: "my-projects",
-          activeBasePath: "my-projects",
+          label: 'My Projects',
+          to: 'my-projects/intro',
+          position: 'left',
+          docsPluginId: 'my-projects',
+          activeBasePath: 'my-projects',
         },
         {
-          label: "My Writings",
-          to: "my-writings/intro",
-          position: "left",
-          docsPluginId: "my-writings",
-          activeBasePath: "my-writings",
+          label: 'My Writings',
+          to: 'my-writings/intro',
+          position: 'left',
+          docsPluginId: 'my-writings',
+          activeBasePath: 'my-writings',
         },
         {
-          label: "Music Projects",
-          to: "music-projects/intro",
-          position: "left",
-          docsPluginId: "music-projects",
-          activeBasePath: "music-projects",
+          label: 'Music Projects',
+          to: 'music-projects/intro',
+          position: 'left',
+          docsPluginId: 'music-projects',
+          activeBasePath: 'music-projects',
         },
-        { to: "/my-blogs", label: "My Blogs", position: "left" },
-        { to: "/my-japanese-blogs", label: "日本語のブログ", position: "left" },
-        { to: "/todo-list", label: "To Do List", position: "left" },
+        { to: '/my-blogs', label: 'My Blogs', position: 'left' },
+        { to: '/my-japanese-blogs', label: '日本語のブログ', position: 'left' },
+        { to: '/todo-list', label: 'To Do List', position: 'left' },
         {
-          href: "https://github.com/takahirohonda/",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/takahirohonda/',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Docs",
+          title: 'Docs',
           items: [
             {
-              label: "My Projects",
-              to: "/my-projects/intro",
+              label: 'My Projects',
+              to: '/my-projects/intro',
             },
             {
-              label: "My Writings",
-              to: "/my-writings/intro",
+              label: 'My Writings',
+              to: '/my-writings/intro',
             },
             {
-              label: "Music Projects",
-              to: "/music-projects/intro",
+              label: 'Music Projects',
+              to: '/music-projects/intro',
             },
             {
-              label: "My Learning Notes",
-              to: "/my-learning-notes/intro",
+              label: 'My Learning Notes',
+              to: '/my-learning-notes/intro',
             },
           ],
         },
         {
-          title: "Connect",
+          title: 'Connect',
           items: [
             {
-              label: "YouTube",
-              href: "https://www.youtube.com/@takahondamusic",
+              label: 'YouTube',
+              href: 'https://www.youtube.com/@takahondamusic',
             },
           ],
         },
         {
-          title: "More",
+          title: 'More',
           items: [
             {
-              label: "Blog",
-              to: "/my-blogs",
+              label: 'Blog',
+              to: '/my-blogs',
             },
             {
-              label: "GitHub",
-              href: "https://github.com/takahirohonda",
+              label: 'GitHub',
+              href: 'https://github.com/takahirohonda',
             },
           ],
         },
@@ -202,11 +204,11 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
     colorMode: {
-      defaultMode: "dark",
+      defaultMode: 'dark',
       disableSwitch: false,
       respectPrefersColorScheme: false,
     },
   } satisfies Preset.ThemeConfig,
-};
+}
 
-export default config;
+export default config
